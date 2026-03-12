6736284c08dc853e29daefc29722e0d5f2b77c59
AkshiiGIT <ton-email-github@exemple.com>
2026-03-12 12:17:09 +0100

    feat: version SaaS Pro avec persistance des clés, nouveau logo et chargement instantané

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'menuai-cache-v2';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './app-icon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/jsQR/1.4.0/jsQR.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
